8cae2f8348c70bf33dbec2429bf50a567b84ba31
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-24 13:48:31 +0900

    Update 出力.py

--- Python基礎/出力.py
+++ Python基礎/出力.py
@@ -19,3 +19,13 @@ number = 7
 
 # 変数 number の値を出力してください
 print(number)
+
+-----------------------------------------------------------
+apple_price = 200
+apple_count = 8
+
+# apple_price と apple_count を掛けた結果を、変数 total_price に代入してください
+total_price = apple_price * apple_count
+
+# total_price の値を出力してください
+print(total_price)
