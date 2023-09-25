9efd07218393b7c963a4b0d8a826d50a132d34ba
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-25 11:09:48 +0900

    Update int型とString型連結.java

--- Java基礎/int型とString型連結.java
+++ Java基礎/int型とString型連結.java
@@ -19,3 +19,16 @@ class Main {
     
   }
 }
+
+//強制的に型変換を行う(キャスト)場合、どちらか一方の前に（double）と入れると、片方がdouble型になり、出力も自動変換で、doubleとなる
+class Main {
+  public static void main(String[] args) {
+    int number1 = 7;
+    int number2 = 2;
+    System.out.println(number1 / number2);
+    
+    // number1をdouble型にキャストし、number2で割った値を出力してください
+    System.out.println((double)number1 / number2);
+    
+  }
+}
