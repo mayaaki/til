2fb362b65d2e4f678d55c503443a6fd5f60b20b3
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-25 11:05:10 +0900

    Update int型とString型連結.java

--- Java基礎/int型とString型連結.java
+++ Java基礎/int型とString型連結.java
@@ -1,1 +1,21 @@
-
+//int とstringの組み合わせであれば、数値がstringと変わる
+//doubleとintの組み合わせであれば自動的にdoubleで出力される
+class Main {
+  public static void main(String[] args) {
+    int month = 12;
+    int date = 31;
+    
+    // 「12月31日」となるように変数と文字列を連結して出力してください
+    System.out.println(month + "月" + date + "日");
+    
+    // 7を2で割った値を出力してください
+    System.out.println(7 / 2);
+    
+    // 7.0を2.0で割った値を出力してください
+    System.out.println(7.0 / 2.0);
+    
+    // 7を2.0で割った値を出力してください
+    System.out.println(7 / 2.0);
+    
+  }
+}
