ab30bf5ff441a8cec907a560ca4bc353dc2454f4
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-26 12:39:34 +0900

    Update 引数.java

--- Java基礎/引数.java
+++ Java基礎/引数.java
@@ -1,3 +1,6 @@
+//メソッド＝工場、発注者＝mainメソッド、処理＝｛｝の中身、生成結果＝戻り値return
+
+
 class Main {
   public static void main(String[] args) {
     // それぞれ年齢に関する引数を追加してください
@@ -15,3 +18,27 @@ class Main {
 }
 
 ------------------------------------------------------------
+class Main {
+  public static void main(String[] args) {
+    // fullNameメソッドの結果を変数nameに代入してください
+    String name = fullName("Kate", "Jones");
+    
+    // printDataの引数を書き換えてください
+    printData(name , 27);
+    
+    // こちらは書き換えないでください
+    printData("John Christopher Smith", 65);
+    
+  }
+
+  public static void printData(String name, int age) {
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+  }
+
+  // fullNameメソッドを定義してください
+  public static String fullName(String firstName, String lastName){
+    return firstName + " " + lastName;
+  }
+}
+  
