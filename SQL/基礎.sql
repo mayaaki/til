978f8083144e172b934853ccf099dfd03dfba574
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-26 14:47:54 +0900

    Update 基礎.sql

--- SQL/基礎.sql
+++ SQL/基礎.sql
@@ -2,3 +2,14 @@
 
 SELECT price
 FROM purchases;
+-----------------------------------------------------------------
+
+//purchasesテーブルから、全てのカラムのデータを取得
+
+SELECT *
+From purchases;
+--------------------------------------------------------------------
+//purchasesテーブルから、「nameカラム」と「priceカラム」のデータを取得
+
+SELECT name, price
+FROM purchases;
