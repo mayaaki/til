003ea48d303f106fa3a2996b015e454e9e1a9595
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-28 15:07:18 +0900

    Update ANDとOR.sql

--- SQL/ANDとOR.sql
+++ SQL/ANDとOR.sql
@@ -1,1 +1,11 @@
+-- categoryカラムが「食費」またはcharacter_nameカラムが「わんこ」であるデータを取得
+SELECT *
+FROM purchases
+WHERE category = "食費"
+OR character_name = "わんこ";
 
+-- categoryカラムが「食費」かつcharacter_nameカラムが「ひつじ」であるデータを取得
+SELECT *
+FROM purchases
+WHERE category = "食費"
+AND character_name = "ひつじ";
