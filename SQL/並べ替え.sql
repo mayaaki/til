584ea8c827b1a4bd04df0929d3ce39eb1f036cdf
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-28 15:17:07 +0900

    Update 並べ替え.sql

--- SQL/並べ替え.sql
+++ SQL/並べ替え.sql
@@ -1,1 +1,10 @@
+-- priceカラムを基準に降順に並び替えたデータを取得
+SELECT *
+FROM purchases
+ORDER BY price DESC;
 
+-- 、priceカラムを基準に昇順に並び替えたデータを取得(WHEREとORDER　BYの併用)
+SELECT *
+FROM purchases
+WHERE character_name = "わんこ"
+ORDER BY price ASC;
