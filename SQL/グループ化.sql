bffee4fe6d6f6e671f434e912fb128336c43c6ae
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-09-29 18:31:52 +0900

    Update グループ化.sql

--- SQL/グループ化.sql
+++ SQL/グループ化.sql
@@ -18,3 +18,10 @@ SELECT SUM(price), purchased_at, character_name
 FROM purchases
 GROUP BY purchased_at, character_name
 ;
+
+-- purchased_atとcharacter_nameごとにお金を使った回数を取得
+
+SELECT COUNT(*), purchased_at, character_name
+FROM purchases
+GROUP BY purchased_at, character_name
+;
