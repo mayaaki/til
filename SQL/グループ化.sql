31e2385653c0233c4b760d2de1e283d1a2637601
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-03 14:13:34 +0900

    Update グループ化.sql

--- SQL/グループ化.sql
+++ SQL/グループ化.sql
@@ -33,3 +33,11 @@ FROM purchases
 WHERE character_name = "わんこ"
 GROUP BY purchased_at
 ;
+
+-- WHEREに条件を付け足してcategoryが食費であるデータを、purchased_atとcharacter_nameでグループ化
+
+SELECT SUM(price), purchased_at, character_name
+FROM purchases
+WHERE category ="食費"
+GROUP BY purchased_at, character_name
+;
