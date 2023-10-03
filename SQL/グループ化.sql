8021c6ea75fc6c7f7fa981df1619d442238af8b2
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-03 14:51:21 +0900

    Update グループ化.sql

--- SQL/グループ化.sql
+++ SQL/グループ化.sql
@@ -49,3 +49,12 @@ FROM purchases
 WHERE category ="食費"
 GROUP BY purchased_at, character_name
 ;
+
+-- 日付ごとの合計金額のうち、2000円を超えるデータのみを取得
+
+SELECT SUM(price), purchased_at
+FROM purchases
+GROUP BY purchased_at
+HAVING SUM(price) > 2000
+
+;
