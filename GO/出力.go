5255c431cd27931ab621af9a65147c667cff5062
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-04 11:24:34 +0900

    Update 出力.go

--- GO/出力.go
+++ GO/出力.go
@@ -5,3 +5,17 @@ func main() {
     println("Hello, Go")
 }
 
+//数値・文字列の出力
+package main
+
+func main() {
+    // 数値の7を出力
+    println(7)
+    
+    // 9に3足した値を出力
+    println(9 + 3)
+    
+    // 「9 + 3」を文字列として出力
+    println("9 + 3")
+    
+}
