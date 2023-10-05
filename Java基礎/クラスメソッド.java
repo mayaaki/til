c3ed847b2078d7f2e9eee16a870ad0f9e136c9f6
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-05 17:34:37 +0900

    Update クラスメソッド.java

--- Java基礎/クラスメソッド.java
+++ Java基礎/クラスメソッド.java
@@ -5,7 +5,6 @@ public class Main {
     Person.printData(Person.fullName("Kate", "Jones"), 27, 1.6, 50.0);
     Person.printData(Person.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
   }
- 
 }
 
 
