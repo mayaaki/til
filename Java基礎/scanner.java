f4b32274100a5d751a47506257694f646bfe35b0
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-09 10:16:35 +0900

    Update scanner.java

--- Java基礎/scanner.java
+++ Java基礎/scanner.java
@@ -1,1 +1,19 @@
+//文字を入力させて出力
 
+// java.util.Scannerを読み込み
+import java.util.Scanner;
+
+class Main {
+  public static void main (String[] args) {
+    //scannerを初期化
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("名前： ");
+    
+    // 変数nameを定義し、コンソールから文字列を受け取って代入
+    String name = scanner.next();
+    
+    // 「こんにちは◯◯さん」と出力
+    System.out.println("こんにちは" + name + "さん");
+  }
+}
