9602a63913d9583451c1f18e3dac664fb53790fb
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-11 21:26:29 +0900

    Update 条件分岐.py

--- Python基礎/条件分岐.py
+++ Python基礎/条件分岐.py
@@ -10,3 +10,14 @@ else:
 
 //elifを使った条件分岐
 //elifはいくらでも使える。
+money = 100
+apple_price = 100
+
+if money > apple_price:
+    print('りんごを買うことができます')
+# 変数の値が等しい場合に「 りんごを買うことができますが所持金が0になります 」と出力してください
+elif money == apple_price:
+    print('りんごを買うことができますが所持金が0になります')
+
+else:
+    print('お金が足りません')
