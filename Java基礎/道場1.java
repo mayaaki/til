a437908c8be32574682c74327ae4fa724bc953bf
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-12 15:06:32 +0900

    Update 道場1.java

--- Java基礎/道場1.java
+++ Java基礎/道場1.java
@@ -57,3 +57,37 @@ class Person{
     return bmi >= 18.5 && bmi < 25.0;
   }
 }
+------------------------------------------------------------------------------------------------------------------------------
+//最高年齢の出力を追加
+//Main.java
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("何人分の情報を入力しますか：");
+    int n = scanner.nextInt();
+    int maxAge = 0;
+    for (int i = 0; i < n; i++){
+      System.out.print(i + 1 + "人目");
+      System.out.print("名前：");
+      String firstName = scanner.next();
+      System.out.print("名字：");
+      String lastName = scanner.next();
+      System.out.print("年齢：");
+      int age = scanner.nextInt();
+      if (age > maxAge){
+        maxAge = age;  //ageがmaxAgeより大きいときはageをmaxAgeに代入
+      }
+      System.out.print("身長(m)：");
+      double height = scanner.nextDouble();
+      System.out.print("体重(kg)：");
+      double weight = scanner.nextDouble();
+      Person.printData(firstName, lastName, age, height, weight);
+    }
+  System.out.println("最高年齢は" + maxAge + "歳です");
+  }
+}
+
+//Person.java
+同上
