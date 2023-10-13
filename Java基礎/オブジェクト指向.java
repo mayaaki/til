4c9a922de9279e4cb48bf6014b4f1f58ab30960e
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-13 11:04:45 +0900

    Update オブジェクト指向.java

--- Java基礎/オブジェクト指向.java
+++ Java基礎/オブジェクト指向.java
@@ -1,1 +1,18 @@
+//Main.java
+//クラスは実体を持たない設計図、インスタンスは実体を持ち、いくつも生成可能。
+class Main {
+  public static void main(String[] args) {
+    // Personクラスのインスタンスを生成し、変数person1に代入
+    Person person1 = new Person();
+    
+    // Personクラスのインスタンスを生成し、変数person2に代入
+    Person person2 = new Person();
+    
+  }
+}
 
+//Person.java
+// Personクラスを定義
+class Person{
+  
+}
