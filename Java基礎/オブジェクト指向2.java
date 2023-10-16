68d04cd130bdaddf36773acb9da23cc7ad002532
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-16 11:29:30 +0900

    Update オブジェクト指向2.java

--- Java基礎/オブジェクト指向2.java
+++ Java基礎/オブジェクト指向2.java
@@ -3,3 +3,10 @@
 //newを使ってインスタンス生成後に自動で呼び出される
 //①コンストラクタ名はクラス名と同じにする、②戻り値を書いてはいけない（voidも書かない）という2つの決まりがある
 
+//Main.java
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    Person person2 = new Person();
+  }
+}
