53af0ce9c269f795a50a76c092a8129f754380aa
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-16 11:08:28 +0900

    Update オブジェクト指向.java

--- Java基礎/オブジェクト指向.java
+++ Java基礎/オブジェクト指向.java
@@ -38,4 +38,17 @@ class Person{
   }
 }
 ---------------------------------------------------------------------
-  
+//thisを使用
+//クラス内のメソッド定義のなかで使える。
+//Main.java
+  class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    person1.name = "Kate Jones";
+    person1.hello();
+
+    Person person2 = new Person();
+    person2.name = "John Christopher Smith";
+    person2.hello();
+  }
+}
