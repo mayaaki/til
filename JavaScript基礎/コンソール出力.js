8c4dae947d15077962333cd53f96af29b7cde8e5
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-17 14:53:44 +0900

    Update コンソール出力.js

--- JavaScript基礎/コンソール出力.js
+++ JavaScript基礎/コンソール出力.js
@@ -2,3 +2,11 @@
 // 「Hello World」とコンソールに出力
 console.log("Hello World");
 
+// 5と3を足した値を出力
+console.log(5 + 3);
+
+// 20から8を引いた値を出力
+console.log(20 - 8);
+
+// "4 + 5" を文字列として出力
+console.log("4 + 5");
