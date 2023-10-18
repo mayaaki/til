8d032d489ab8141293c574d61eae09ea57498c53
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-18 15:15:36 +0900

    Update 演算.php

--- PHP基礎/演算.php
+++ PHP基礎/演算.php
@@ -1,1 +1,48 @@
+//演算
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
 
+  <?php
+
+    $x = 5;
+    $y = 2;
+    $a = 8;
+    $b = 4;
+    
+  ?>
+
+  <?php
+    $x += 10;
+    echo $x;
+  ?>
+
+  <br>
+
+  <?php
+    $y *= 5;
+    echo $y;
+  ?>
+
+  <br>
+
+  <?php
+    $a ++;
+    echo $a;
+  ?>
+
+  <br>
+  
+  <?php
+    $b --;
+    echo $b;
+  ?>
+
+</body>
+</html>
+--------------------------------------------------------------------------------
