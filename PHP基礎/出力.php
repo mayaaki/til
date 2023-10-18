4d8be6321d616b4a34fad96cced33ec441fff95c
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-18 14:43:14 +0900

    Create 出力.php

---
+++ PHP基礎/出力.php
@@ -0,0 +1,32 @@
+//基礎的な出力
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // 'Hello, world!'をecho
+    echo 'Hello, world!';
+  ?>
+  
+  <br>
+
+  <?php
+    // 7 * 2をecho
+    echo 7 * 2;
+  ?>
+
+  <br>
+
+  <?php
+    // 8 % 3をecho
+    echo 8 % 3;
+  ?>
+
+</body>
+</html>
+--------------------------------------------------------------------
