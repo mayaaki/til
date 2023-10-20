32a210da6fc6eb706448578b629b8f20ae18915d
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-20 16:02:09 +0900

    Update オブジェクト指向2.java

--- Java基礎/オブジェクト指向2.java
+++ Java基礎/オブジェクト指向2.java
@@ -40,3 +40,22 @@ class Person {
   }
 }
 
+//Person.java
+class Person {
+  // 以下にインスタンスフィールドを定義
+  public String firstName;
+  public String lastName;
+  public int age;
+  public double height;
+  public double weight;
+
+  // 以下にコンストラクタを定義し、インスタンスフィールドに値をセット
+  Person(String firstName, String lastName, int age, double height, double weight){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+  
+}
