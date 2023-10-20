a7adb0f099a5ee85f8f808f951329118d76a8acb
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-20 15:45:42 +0900

    Update オブジェクト指向2.java

--- Java基礎/オブジェクト指向2.java
+++ Java基礎/オブジェクト指向2.java
@@ -6,8 +6,8 @@
 //Main.java
 class Main {
   public static void main(String[] args) {
-    Person person1 = new Person(); //インスタンス生成時点で呼び出されるのがコンストラクタ
-    Person person2 = new Person(); //同上
+    Person person1 = new Person("Kate Jones"); //インスタンス生成時点で呼び出されるのがコンストラクタ
+    Person person2 = new Person("John Christopher Smith"); //同上
   }
 }
 
@@ -16,11 +16,14 @@ class Person {
   public String name;
 
   // コンストラクタを定義
-  Person(){
+  Person(String name){ 　　　　　　　　　　　　　　　　　　//Main.javaのnameの引数を受け取る
     System.out.println("インスタンスが生成されました");
+    // インスタンスフィールドnameに値をセット
+    this.name = name;
   }
 
   public void hello() {
     System.out.println("こんにちは、私は" + this.name + "です");
   }
 }
+---------------------------------------------------------------------
