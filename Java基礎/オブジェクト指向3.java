13867731836f6b64d55714630eab6a938384caf3
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-23 13:10:31 +0900

    Update オブジェクト指向3.java

--- Java基礎/オブジェクト指向3.java
+++ Java基礎/オブジェクト指向3.java
@@ -1,3 +1,17 @@
 //クラスフィールドの設定
 //インスタンスフィールドとは違い、クラスフィールドはクラスに属する
 //クラスフィールドの定義は、staticを用いる
+
+//Main.java
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    person1.printData();
+    
+    Person person2 = new Person("John", "Smith", 65, 1.75, 80.0);
+    person2.printData();
+    
+    // 「合計◯◯人です」と出力 クラス名.クラスフィールド名で出力
+    System.out.println("合計" + Person.count + "人です");    
+  }
+}
