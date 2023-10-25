64e65425f93cbdfca037958a2a11182e17fdadc1
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-25 17:15:24 +0900

    Update オブジェクト指向3.java

--- Java基礎/オブジェクト指向3.java
+++ Java基礎/オブジェクト指向3.java
@@ -168,4 +168,53 @@ class Person {
     return this.weight / this.height / this.height;
   }
 ------------------------------------------------------------------------------------------------------
-  //上記重複部分をthisを用いてコンストラクタを呼び出す
+//上記重複部分をthisを用いてコンストラクタを呼び出す
+//Main.java
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    person1.printData();
+    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
+    person2.printData();
+    Person.printCount();
+  }
+}
+
+//Person.java
+class Person {
+  public static int count = 0;
+  public String firstName;
+  public String middleName;
+  public String lastName;
+  public int age;
+  public double height, weight;
+
+  Person(String firstName, String lastName, int age, double height, double weight) {
+    Person.count++;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+
+  Person(String firstName, String middleName, String lastName, int age, double height, double weight) {
+    // this()を用いて、コンストラクタを呼び出す
+    this(firstName, lastName, age, height, weight);
+    
+    this.middleName = middleName;
+  }
+
+  public String fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  public void printData() {
+    System.out.println("私の名前は" + this.fullName() + "です");
+    System.out.println("年齢は" + this.age + "歳です");
+    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
+  }
+
+  public double bmi() {
+    return this.weight / this.height / this.height;
+  }
