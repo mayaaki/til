499c86d2205511bb86eb417e6106f35a19c604a4
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-25 16:54:10 +0900

    Update オブジェクト指向3.java

--- Java基礎/オブジェクト指向3.java
+++ Java基礎/オブジェクト指向3.java
@@ -123,4 +123,49 @@ class Main {
   }
 }
 
+//Person.java
+class Person {
+  public static int count = 0;
+  public String firstName;
+  // インスタンスフィールドmiddleNameを定義
+  public String middleName;
+  
+  public String lastName;
+  public int age;
+  public double height, weight;
+
+  Person(String firstName, String lastName, int age, double height, double weight) {
+    Person.count++;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+  
+  // middleNameを受け取るコンストラクタを定義
+  Person(String firstName, String middleName, String lastName, int age, double height, double weight) {
+    Person.count++;
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+  }
+
+  public String fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+  public void printData() {
+    System.out.println("私の名前は" + this.fullName() + "です");
+    System.out.println("年齢は" + this.age + "歳です");
+    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
+  }
+
+  public double bmi() {
+    return this.weight / this.height / this.height;
+  }
+
   
