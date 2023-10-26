762b1052c31e1e54299fb147f334b1deead5f51f
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-26 16:28:15 +0900

    Update 配列.php

--- PHP基礎/配列.php
+++ PHP基礎/配列.php
@@ -21,5 +21,26 @@
 
 </body>
 </html>
-
+-----------------------------------------------------------------------------------------
 //連想配列
+//index.php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 下記が配列
+    $scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+    $scores['国語'] += 5;
+    echo $scores['国語'];
+    
+  ?>
+
+</body>
+</html>
