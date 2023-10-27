03ad06c1f5cc009b3fb5f4c4d4a49d61d1b1fdb1
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-27 13:18:43 +0900

    Update オブジェクト指向4.java

--- Java基礎/オブジェクト指向4.java
+++ Java基礎/オブジェクト指向4.java
@@ -11,3 +11,15 @@ class Person {
 
 //フィールドをprivateにしたうえで、クラス外から安全にフィールの値を取得するゲッターを使用
 //ゲッターは、「getフィールド名」のように命名するのが一般的です
+
+//Main.java
+ // person2に対して、getMiddleNameメソッドを呼び出し、「person2のミドルネームは◯◯です」と出力
+    System.out.println("person2のミドルネームは" + person2.getMiddleName() + "です");
+//Person.java
+  class Person {
+  private String middleName;
+    // middleNameフィールドのゲッターを定義
+    public String getMiddleName(){
+      return this.middleName;
+    }
+  }
