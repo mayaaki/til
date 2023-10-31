018fd836fd7ed5b5f8caf688946193a5a87ec7fa
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-31 17:04:49 +0900

    Update オブジェクト指向道場.java

--- Java基礎/オブジェクト指向道場.java
+++ Java基礎/オブジェクト指向道場.java
@@ -31,3 +31,19 @@ class Bicycle {
    }            
  }
 ------------------------------------------------------------------------------------------
+//上記をprintDataメソッドでまとめ、Main.javaからBicycle.javaへ移す
+//printDaraメソッドでまとめて移行したことで、ゲッターで取得の必要がなくなるので消す
+
+//Main.java
+class Main {
+  public static void main(String[] args) {
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+    System.out.println("【自転車の情報】");
+
+    //上記のBicycle bicycleにおけるprintDataの呼び出し
+    bicycle.printData();
+  }
+}
+
+
+
