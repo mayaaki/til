bd80140be7979ca813a19802c3e827f1c805025d
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-31 17:05:37 +0900

    Update オブジェクト指向道場.java

--- Java基礎/オブジェクト指向道場.java
+++ Java基礎/オブジェクト指向道場.java
@@ -45,5 +45,19 @@ class Main {
   }
 }
 
-
+//Bicycle.java
+class Bicycle {
+  private String name;
+  private String color;
+  
+  Bicycle(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  public void printData() {
+    System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
+  }
+}
 
