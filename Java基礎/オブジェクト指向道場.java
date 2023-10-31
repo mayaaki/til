ac4ff0bc21d10b59de1fadecb6b3ef1ea521fe62
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-10-31 16:12:05 +0900

    Update オブジェクト指向道場.java

--- Java基礎/オブジェクト指向道場.java
+++ Java基礎/オブジェクト指向道場.java
@@ -2,4 +2,15 @@
 //インスタンスを生成
 //コンストラクタでインスタンス生成時に実行
 
-
+//Main.java
+class Main {
+  public static void main(String[] args) {
+    //インスタンス生成
+    Bicycle bicycle = new Bicycle("ビアンキ");
+    
+    System.out.println("【自転車の情報】");
+    
+    //ゲッターを使って、プライベート変数を取得
+    System.out.println("名前：" + bicycle.getName());
+  }
+}
