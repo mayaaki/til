9ce90a71c27ef73ff0821721634abdc861b5462a
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-02 16:10:25 +0900

    Update オブジェクト指向道場2.java

--- Java基礎/オブジェクト指向道場2.java
+++ Java基礎/オブジェクト指向道場2.java
@@ -22,3 +22,27 @@ class Main {
     car.printData();
   }
 }
+
+//Car.java
+class Car {
+  private String name;
+  private String color;
+  private int distance = 0;
+  
+  Car(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  public void printData() {
+    System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+  
+  public void run(int runDistance) {
+    System.out.println(runDistance + "km走ります");
+    this.distance += runDistance;
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+}
