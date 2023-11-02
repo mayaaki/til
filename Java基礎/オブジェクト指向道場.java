8f81ed77d2570248dd9cb150cfba95393c14e604
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-02 15:53:44 +0900

    Update オブジェクト指向道場.java

--- Java基礎/オブジェクト指向道場.java
+++ Java基礎/オブジェクト指向道場.java
@@ -158,6 +158,30 @@ class Main {
   }
 }
 
+//Bicycle.java
+class Bicycle {
+  private String name;
+  private String color;
+  private int distance = 0;
+  
+  Bicycle(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  public void printData() {
+    System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+  
+  public void run(int runDistance) {
+    System.out.println(runDistance + "km走ります");
+    this.distance += runDistance;
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+}
+
 
 
 
