4d473989248b4c2630939d365a6a9f2db97e6ad4
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-07 14:53:40 +0900

    Update オブジェクト指向道場2.java

--- Java基礎/オブジェクト指向道場2.java
+++ Java基礎/オブジェクト指向道場2.java
@@ -145,7 +145,7 @@ class Car {
 //給油メソッドの追加
 
 //Main.java
-  import java.util.Scanner;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
@@ -169,6 +169,51 @@ class Main {
     System.out.print("給油する量を入力してください：");
     int charge = scanner.nextInt();
     car.charge(charge);
+  }
+}
+
+//Car.java
+class Car {
+  private String name;
+  private String color;
+  private int distance = 0;
+  private int fuel = 100;
+  
+  Car(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  public void printData() {
+    System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "km");
+    System.out.println("ガソリン量：" + this.fuel + "L");
+  }
+  
+  public void run(int runDistance) {
+    System.out.println(runDistance + "km走ります");
+    if(runDistance <= this.fuel) {
+    this.distance += runDistance;
+    this.fuel -= runDistance;
+    }else{
+      System.out.println("ガソリンが足りません");
+    }
+    System.out.println("走行距離：" + this.distance + "km");
+    System.out.println("ガソリン量：" + this.fuel + "L");
+  }
+  
+  public void charge(int charge) {
+    System.out.println(charge + "L給油します");
+    if(charge <= 0) {
+      System.out.println("給油できません");
+    }else if(charge + this.fuel >= 100) {
+      System.out.println("満タンまで給油します");
+      this.fuel = 100;
+    }else{
+      this.fuel += charge;
+    }
+    System.out.println("ガソリン量：" + this.fuel + "L");
   }
 }
 
