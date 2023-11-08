8073076d3bcbd55b4da4cf194151047bf39c4142
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-08 11:09:44 +0900

    Update 継承.java

--- Java基礎/継承.java
+++ Java基礎/継承.java
@@ -1,3 +1,13 @@
 //継承
 //スーパークラスとサブクラス
 //新たにスーパークラスを作って継承する
+
+//Main.java
+class Main {
+  public static void main(String[] args) {
+    Car car = new Car();
+    Bicycle bicycle = new Bicycle();
+  }
+}
+
+
