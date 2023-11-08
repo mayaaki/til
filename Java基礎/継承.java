e113a744b132c93f67fd6ea4e1286a9245091021
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-08 11:09:58 +0900

    Update 継承.java

--- Java基礎/継承.java
+++ Java基礎/継承.java
@@ -10,4 +10,32 @@ class Main {
   }
 }
 
+//Vehicle.java
+class Vehicle{
+  private String name;
+  private String color;
+  private int distance = 0;
+
+  public String getName() {
+    return this.name;
+  }
+  public String getColor() {
+    return this.color;
+  }
+  public int getDistance() {
+    return this.distance;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+  public void printData() {
+    System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+}
+
 
