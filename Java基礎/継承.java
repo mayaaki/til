33ebb4abd69ed31553fe2d781c8d7d4c376719fa
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-10 17:18:55 +0900

    Update 継承.java

--- Java基礎/継承.java
+++ Java基礎/継承.java
@@ -230,3 +230,36 @@ class Vehicle {
 }
 
 
+//Car.java
+//サブクラス、Bicycle.java（サブクラス）は省略
+class Car extends Vehicle {
+  private int fuel = 50;
+
+  public int getFuel() {
+    return this.fuel;
+  }
+  
+  // printDataメソッドを定義(オーバーライドで上書き、スーパークラスに探しに行き(this)プライベートメソッドがあるのでゲッターを使用で取得)
+  public void printData(){
+    System.out.println("名前：" + this.getName());
+    System.out.println("色：" + this.getColor());
+    System.out.println("走行距離：" + this.getDistance() + "km");
+    System.out.println("ガソリン量：" + this.fuel + "L");
+  }
+
+  public void charge(int litre) {
+    System.out.println(litre + "L給油します");
+    if (litre <= 0) {
+      System.out.println("給油できません");
+    } else if (litre + this.fuel >= 100) {
+      System.out.println("満タンまで給油します");
+      this.fuel = 100;
+    } else {
+      this.fuel += litre;
+    }
+    System.out.println("ガソリン量：" + this.fuel + "L");
+  }
+}
+-------------------------------------------------------------------------------------------------------------------------------
+
+
