53bb1870d067fe38e3dc76b9dd9d0793972aef92
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-14 14:01:07 +0900

    Update protected.java

--- Java基礎/protected.java
+++ Java基礎/protected.java
@@ -30,5 +30,38 @@ class Main {
     int bicycleDistance = scanner.nextInt();
     // runメソッドを呼び出して、引数にbicycleDistanceを渡しす
     bicycle.run(bicycleDistance);
-  
   }
+
+//Vehicle.java
+class Vehicle {
+  private String name;
+  private String color;
+  // protectedを用いる
+  protected int distance = 0;
+
+  Vehicle(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+  public String getColor() {
+    return this.color;
+  }
+  public int getDistance() {
+    return this.distance;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setColor(String color) {
+    this.color = color;
+  }
+  public void printData() {
+    System.out.println("名前：" + this.name);
+    System.out.println("色：" + this.color);
+    System.out.println("走行距離：" + this.distance + "(km)");
+  }
+}
