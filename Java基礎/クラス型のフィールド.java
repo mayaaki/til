9459d898bf6cd181166e3270d8a02d1771a6e068
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-16 18:52:30 +0900

    Update クラス型のフィールド.java

--- Java基礎/クラス型のフィールド.java
+++ Java基礎/クラス型のフィールド.java
@@ -141,3 +141,34 @@ class Main {
 -----------------------------------------------------------------------
 //buyメソッド
 //buyメソッドの中で、引数に受け取ったインスタンスのセッターを用いて所有者を変更する
+
+//Main.java
+class Main {
+  public static void main(String[] args) {
+    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
+
+    Car car = new Car("フェラーリ", "赤");
+    Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+    
+    // buyメソッドを用いて、person1にcarを購入
+   person1.buy(car);
+    
+    // buyメソッドを用いて、person2にbicycleを購入
+    person2.buy(bicycle);
+
+    System.out.println("【車の情報】");
+    car.printData();
+    System.out.println("-----------------");
+    System.out.println("【車の所有者の情報】");
+    car.getOwner().printData();
+
+    System.out.println("=================");
+    System.out.println("【自転車の情報】");
+    bicycle.printData();
+    System.out.println("-----------------");
+    System.out.println("【自転車の所有者の情報】");
+    bicycle.getOwner().printData();
+  }
+}
+
