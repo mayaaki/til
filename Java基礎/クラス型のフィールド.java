e61df860d3a93466bff5f39663b06588699ecda1
mayaaki <140797962+mayaaki@users.noreply.github.com>
2023-11-16 18:54:28 +0900

    Update クラス型のフィールド.java

--- Java基礎/クラス型のフィールド.java
+++ Java基礎/クラス型のフィールド.java
@@ -268,3 +268,17 @@ abstract class Vehicle {
 
   public abstract void run(int distance);
 }
+
+//Bicycle.java
+class Bicycle extends Vehicle {
+  Bicycle(String name, String color) {
+    super(name, color);
+  }
+
+  public void run(int distance) {
+    System.out.println(distance + "km走ります");
+    this.distance += distance;
+    System.out.println("走行距離：" + this.distance + "km");
+  }
+}
+
